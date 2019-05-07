a8df10e76edaeeb5ee33f51afa8c6201e3322871
syyang <syyang@hyperwallet.com>
2019-05-07 14:30:41 -0700

    Fixed broken tests for ios12

--- Sources/TransferMethod/SelectTransferMethodTypePresenter.swift
+++ Sources/TransferMethod/SelectTransferMethodTypePresenter.swift
@@ -17,7 +17,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import HyperwalletSDK
-import UIKit
 
 protocol SelectTransferMethodTypeView: class {
     func showGenericTableView(items: [CountryCurrencyCellConfiguration],
