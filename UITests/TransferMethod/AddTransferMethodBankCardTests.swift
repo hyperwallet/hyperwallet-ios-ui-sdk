ddaa908ea42060f528fe618e1070586f1b8513e7
Oleksii Lubianyi <Oleksii_Lubianyi@epam.com>
2019-05-15 18:04:17 +0300

    Tests implementation for HW-52181: Update UI Automation - Bank Card

--- UITests/TransferMethod/AddTransferMethodBankCardTests.swift
+++ UITests/TransferMethod/AddTransferMethodBankCardTests.swift
@@ -14,21 +14,7 @@ class AddTransferMethodBankCardTests: BaseTests {
         mockServer.tearDown()
     }
 
-    private func setUpAddTransferMethodBankCardScreen() {
-        selectTransferMethodType = SelectTransferMethodType(app: app)
-        addTransferMethod = AddTransferMethod(app: app, for: .debitCard)
-
-        app.tables.cells.containing(.staticText, identifier: "Add Transfer Method").element(boundBy: 0).tap()
-        spinner = app.activityIndicators["activityIndicator"]
-        waitForNonExistence(spinner)
-        selectTransferMethodType.selectCountry(country: "United States")
-        selectTransferMethodType.selectCurrency(currency: "US Dollar")
-
-        app.tables["transferMethodTableView"].staticTexts.element(matching: debitCard).tap()
-        waitForNonExistence(spinner)
-    }
-
-    func testAddTransferMethod_createDebitCard() {
+    func testAddTransferMethod_createBankCard() {
         mockServer.setupStub(url: "/rest/v3/users/usr-token/bank-cards",
                              filename: "BankCardResponse",
                              method: HTTPMethod.post)
@@ -44,6 +30,22 @@ class AddTransferMethodBankCardTests: BaseTests {
         //Todo - check process indicator 
     }
 
+    func testAddTransferMethod_createBankCardBusiness() {
+        mockServer.setupStub(url: "/rest/v3/users/usr-token/bank-cards",
+                             filename: "BankCardResponse",
+                             method: HTTPMethod.post)
+        
+        addTransferMethod.setCardNumber("4895142232120006")
+        addTransferMethod.setDateOfExpiry(expiryMonth: "March", expiryYear: "2020")
+        
+        XCTAssertEqual(app.textFields["dateOfExpiry"].value as? String, "03/20")
+        
+        addTransferMethod.setCvv("022")
+        addTransferMethod.clickCreateTransferMethodButton()
+        
+        //Todo - check process indicator
+    }
+    
     func testAddTransferMethod_returnsErrorOnInvalidPattern() {
         addTransferMethod.setCardNumber("1234567890@#$")
         XCTAssert(app.tables["addTransferMethodTable"].staticTexts["label_cardNumber_error"].exists)
@@ -102,5 +104,64 @@ class AddTransferMethodBankCardTests: BaseTests {
         XCTAssert(app.tables["addTransferMethodTable"]
             .staticTexts["Transaction Fees: USD 1.75 Processing Time: 30 minutes"]
             .exists)
+    }
+    
+    func testAddTransferMethod_displaysElementsOnIndividualProfileTmcResponse() {
+        XCTAssert(app.navigationBars.staticTexts["Debit Card"].exists)
+        
+        verifyAccountInformationSection()
+        
+        let infoSection = addTransferMethod.addTMTableView.otherElements["TRANSFER METHOD INFORMATION"]
+        
+        addTransferMethod.addTMTableView.scrollToElement(element: infoSection)
+        XCTAssert(infoSection.exists)
+        
+        addTransferMethod.addTMTableView.scrollToElement(element: addTransferMethod.createTransferMethodButton)
+        XCTAssert(addTransferMethod.createTransferMethodButton.exists)
+    }
+    
+    func testAddTransferMethod_displaysElementsOnBusinessProfileTmcResponse() {
+        XCTAssert(app.navigationBars.staticTexts["Debit Card"].exists)
+        
+        verifyAccountInformationSection()
+        
+        let infoSection = addTransferMethod.addTMTableView.otherElements["TRANSFER METHOD INFORMATION"]
+        
+        addTransferMethod.addTMTableView.scrollToElement(element: infoSection)
+        XCTAssert(infoSection.exists)
+        
+        addTransferMethod.addTMTableView.scrollToElement(element: addTransferMethod.createTransferMethodButton)
+        XCTAssert(addTransferMethod.createTransferMethodButton.exists)
+    }
+}
+
+private extension AddTransferMethodBankCardTests {
+    func setUpAddTransferMethodBankCardScreen() {
+        selectTransferMethodType = SelectTransferMethodType(app: app)
+        addTransferMethod = AddTransferMethod(app: app, for: .debitCard)
+
+        app.tables.cells.containing(.staticText, identifier: "Add Transfer Method").element(boundBy: 0).tap()
+        spinner = app.activityIndicators["activityIndicator"]
+        waitForNonExistence(spinner)
+        selectTransferMethodType.selectCountry(country: "United States")
+        selectTransferMethodType.selectCurrency(currency: "US Dollar")
+
+        app.tables["transferMethodTableView"].staticTexts.element(matching: debitCard).tap()
+        waitForNonExistence(spinner)
+    }
+    
+    func verifyAccountInformationSection() {
+        XCTAssert(addTransferMethod.title.exists)
+        
+        XCTAssert(addTransferMethod.addTMTableView.cells.staticTexts["Card Number"].exists)
+        XCTAssert(addTransferMethod.cardNumberInput.exists)
+        
+        XCTAssert(addTransferMethod.addTMTableView.staticTexts["Expiry Date"].exists)
+        XCTAssert(addTransferMethod.dateOfExpiryInput.exists)
+        
+        XCTAssert(addTransferMethod.addTMTableView.staticTexts["CVV (Card Security Code)"].exists)
+        XCTAssert(addTransferMethod.cvvInput.exists)
+        
+        XCTAssert(addTransferMethod.addTMTableView.staticTexts["CVV is a 3 digit number, typically found on the back of the card."].exists)
     }
 }
