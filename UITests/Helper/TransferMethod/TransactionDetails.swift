897a675dda3033c2c7ff160494a265ecbd46c90d
Rita Law <malaw@paypal.com>
2019-06-17 15:34:35 -0700

    HW-53264 Added UI Test for Receipt detail view

---
+++ UITests/Helper/TransferMethod/TransactionDetails.swift
@@ -0,0 +1,47 @@
+import XCTest
+
+
+class TransactionDetails {
+
+    var receiptdetailtableviewTable:XCUIElement
+    var detailSection:XCUIElement
+    var feeSection:XCUIElement
+    var transactionSection:XCUIElement
+    var cellTextLabel: XCUIElement
+    var detailTextLabel: XCUIElement
+    var detailHeaderTitle:XCUIElement
+
+    // Detail
+    var receiptIdLabel:XCUIElement
+    var dateLabel:XCUIElement
+    var clientTransactionIdLabel:XCUIElement
+    // Fee
+    var amountLabel: XCUIElement
+    var feeLabel: XCUIElement
+    var transactionLabel: XCUIElement
+    var backButton: XCUIElement
+
+    var app: XCUIApplication
+
+    init(app:XCUIApplication) {
+        self.app = app
+        detailHeaderTitle = app.navigationBars["Transaction Details"].otherElements["Transaction Details"]
+        receiptdetailtableviewTable = app.tables["receiptDetailTableView"]
+        // "ListReceiptTableViewCellTextLabel"
+        cellTextLabel = receiptdetailtableviewTable.staticTexts["ListReceiptTableViewCellTextLabel"]
+        detailTextLabel = receiptdetailtableviewTable.staticTexts["ListReceiptTableViewDetailTextLabel"]
+        detailSection = receiptdetailtableviewTable.staticTexts["Details"]
+        feeSection  = receiptdetailtableviewTable.staticTexts["Fee Specification"]
+        transactionSection  = receiptdetailtableviewTable.staticTexts["Transaction"]
+        receiptIdLabel = receiptdetailtableviewTable.staticTexts["Receipt ID:"]
+        dateLabel = receiptdetailtableviewTable.staticTexts["Receipt ID:"]
+        clientTransactionIdLabel = receiptdetailtableviewTable.staticTexts["Receipt ID:"]
+        amountLabel = receiptdetailtableviewTable.staticTexts["Amount:"]
+        feeLabel = receiptdetailtableviewTable.staticTexts["Fee:"]
+        transactionLabel = receiptdetailtableviewTable.staticTexts["Transaction:"]
+        backButton = app.navigationBars["Transaction Details"].buttons["Back"]
+    }
+
+    
+    
+}
