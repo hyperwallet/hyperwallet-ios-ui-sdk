b53af8b7bed122c10584a3fec448efc9dc070aef
Rita Law <malaw@paypal.com>
2019-06-27 14:22:35 -0700

    HW-53820 Tests are working at this point

--- UITests/Receipts/ListReceiptTests.swift
+++ UITests/Receipts/ListReceiptTests.swift
@@ -142,59 +142,62 @@ class ListReceiptTests: BaseTests {
     func testPrepaidCardReceiptsList_verifyReceiptsOrder() {
         let expectedNumberOfCells = 4
 
-        mockServer.setupStub(url: "/rest/v3/users/usr-token/receipts",
+        mockServer.setupStub(url: "/rest/v3/users/usr-token/prepaid-cards/trm-token/receipts",
                              filename: "PrepaidCardReceiptsForOneMonth",
                              method: HTTPMethod.get)
+        let prepaidCardMenu = "List Prepaid Card Receipts"
+        let prepaidCardReceiptMenu = app.tables.cells.containing(.staticText, identifier: prepaidCardMenu).element(boundBy: 0)
 
-        openReceiptsListScreen()
+        XCTAssertTrue(prepaidCardReceiptMenu.exists)
+        prepaidCardReceiptMenu.tap()
 
         XCTAssertEqual(app.tables.cells.count, expectedNumberOfCells)
 
         if #available(iOS 12, *) {
-            verifyCellExists(with: "Balance Adjustment\nJun 24, 2019", moneyTitle: "-500.99\nUSD", by: 3)
-            verifyCellExists(with: "Balance Adjustment\nJun 23, 2019", moneyTitle: "-7.00\nUSD", by: 2)
-            verifyCellExists(with: "Funds Deposit\nJun 21, 2019", moneyTitle: "+20.00\nUSD", by: 1)
             verifyCellExists(with: "Funds Deposit\nJun 20, 2019", moneyTitle: "+10.00\nUSD", by: 0)
+            verifyCellExists(with: "Funds Deposit\nJun 21, 2019", moneyTitle: "+20.00\nUSD", by: 1)
+            verifyCellExists(with: "Balance Adjustment\nJun 23, 2019", moneyTitle: "-7.00\nUSD", by: 2)
+            verifyCellExists(with: "Balance Adjustment\nJun 24, 2019", moneyTitle: "-500.99\nUSD", by: 3)
         } else {
-            verifyCellExists(with: "Balance Adjustment Jun 24, 2019", moneyTitle: "-500.99 USD", by: 3)
-            verifyCellExists(with: "Balance Adjustment Jun 23, 2019", moneyTitle: "-7.00 USD", by: 2)
-            verifyCellExists(with: "Funds Deposit Jun 21, 2019", moneyTitle: "+20.00 USD", by: 1)
             verifyCellExists(with: "Funds Deposit Jun 20, 2019", moneyTitle: "+10.00 USD", by: 0)
+            verifyCellExists(with: "Funds Deposit Jun 21, 2019", moneyTitle: "+20.00 USD", by: 1)
+            verifyCellExists(with: "Balance Adjustment Jun 23, 2019", moneyTitle: "-7.00 USD", by: 2)
+            verifyCellExists(with: "Balance Adjustment Jun 24, 2019", moneyTitle: "-500.99 USD", by: 3)
         }
     }
 
     func testReceiptsList_verifyPrepaidCardReceiptsSectionHeaders() {
-        mockServer.setupStub(url: "/rest/v3/users/usr-token/receipts",
-                                     filename: "PrepaidCardReceiptsForFewMonths",
-                                     method: HTTPMethod.get)
+        mockServer.setupStub(url: "/rest/v3/users/usr-token/prepaid-cards/trm-token/receipts",
+                             filename: "PrepaidCardReceiptsForFewMonths",
+                             method: HTTPMethod.get)
+        let prepaidCardMenu = "List Prepaid Card Receipts"
+        let prepaidCardReceiptMenu = app.tables.cells.containing(.staticText, identifier: prepaidCardMenu).element(boundBy: 0)
 
-        openReceiptsListScreen()
-        XCTAssertTrue(app.tables.staticTexts["June 2019"].exists)
-        XCTAssertTrue(app.tables.staticTexts["May 2019"].exists)
+        XCTAssertTrue(prepaidCardReceiptMenu.exists)
+        prepaidCardReceiptMenu.tap()
+
         XCTAssertTrue(app.tables.staticTexts["April 2019"].exists)
+        XCTAssertTrue(app.tables.staticTexts["May 2019"].exists)
+        XCTAssertTrue(app.tables.staticTexts["June 2019"].exists)
     }
 
     func testPrepaidCardReceiptsList_verifyPagingBySwipe() {
-        mockServer.setupStub(url: "/rest/v3/users/usr-token/receipts",
-                             filename: "PrepaidCardReceiptsMoreThan20",
+        mockServer.setupStub(url: "/rest/v3/users/usr-token/prepaid-cards/trm-token/receipts",
+                             filename: "PrepaidCardReceiptsPaging",
                              method: HTTPMethod.get)
-        openReceiptsListScreen()
+        let prepaidCardMenu = "List Prepaid Card Receipts"
+        let prepaidCardReceiptMenu = app.tables.cells.containing(.staticText, identifier: prepaidCardMenu).element(boundBy: 0)
 
-        if #available(iOS 12, *) {
-            verifyCellDoesNotExist(with: "Funds Deposit\nApril 01, 2019", moneyTitle: "+10.00\nUSD", by: 21)
-        } else {
-            verifyCellDoesNotExist(with: "Funds Deposit April 01, 2019", moneyTitle: "+10.00 USD", by: 21)
-        }
+        XCTAssertTrue(prepaidCardReceiptMenu.exists)
+        prepaidCardReceiptMenu.tap()
 
         app.swipeUp()
         app.swipeUp()
-        app.swipeUp()
-        waitForNonExistence(spinner)
-        if #available(iOS 12, *) {
-            verifyCellExists(with: "Funds Deposit\nApril 01, 2019", moneyTitle: "+10.00\nUSD", by: 21)
-        } else {
-            verifyCellExists(with: "Funds Deposit April 01, 2019", moneyTitle: "+10.00 USD", by: 21)
-        }
+        let lastRow = app.tables.element.children(matching: .cell).element(boundBy: 10)
+        let lastRowLabel = lastRow.staticTexts["ListReceiptTableViewCellTextLabel"].label
+
+        XCTAssertTrue(lastRow.exists)
+        XCTAssertTrue(lastRowLabel.contains("Jun 8, 2019"))
     }
 
     // MARK: helper functions
