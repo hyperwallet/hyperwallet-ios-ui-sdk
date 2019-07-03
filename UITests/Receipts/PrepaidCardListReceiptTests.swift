8dc6dec3f8028bbd16ca060b1ef93bd0bbd317ed
Rita Law <malaw@paypal.com>
2019-07-03 15:42:38 -0700

    remove + for credit  based on changes of HW-53999

--- UITests/Receipts/PrepaidCardListReceiptTests.swift
+++ UITests/Receipts/PrepaidCardListReceiptTests.swift
@@ -28,13 +28,13 @@ class PrepaidCardListReceiptTests: BaseTests {
         XCTAssertEqual(app.tables.cells.count, expectedNumberOfCells)
 
         if #available(iOS 12, *) {
-            verifyCellLabels(with: "Funds Deposit\nJun 20, 2019", moneyTitle: "+10.00\nUSD", by: 0)
-            verifyCellLabels(with: "Funds Deposit\nJun 21, 2019", moneyTitle: "+20.00\nUSD", by: 1)
+            verifyCellLabels(with: "Funds Deposit\nJun 20, 2019", moneyTitle: "10.00\nUSD", by: 0)
+            verifyCellLabels(with: "Funds Deposit\nJun 21, 2019", moneyTitle: "20.00\nUSD", by: 1)
             verifyCellLabels(with: "Balance Adjustment\nJun 23, 2019", moneyTitle: "-7.00\nUSD", by: 2)
             verifyCellLabels(with: "Balance Adjustment\nJun 24, 2019", moneyTitle: "-500.99\nUSD", by: 3)
         } else {
-            verifyCellLabels(with: "Funds Deposit Jun 20, 2019", moneyTitle: "+10.00 USD", by: 0)
-            verifyCellLabels(with: "Funds Deposit Jun 21, 2019", moneyTitle: "+20.00 USD", by: 1)
+            verifyCellLabels(with: "Funds Deposit Jun 20, 2019", moneyTitle: "10.00 USD", by: 0)
+            verifyCellLabels(with: "Funds Deposit Jun 21, 2019", moneyTitle: "20.00 USD", by: 1)
             verifyCellLabels(with: "Balance Adjustment Jun 23, 2019", moneyTitle: "-7.00 USD", by: 2)
             verifyCellLabels(with: "Balance Adjustment Jun 24, 2019", moneyTitle: "-500.99 USD", by: 3)
         }
@@ -83,9 +83,9 @@ class PrepaidCardListReceiptTests: BaseTests {
         waitForNonExistence(spinner)
 
         if #available(iOS 12, *) {
-            verifyCellLabels(with: "Funds Deposit\nJun 20, 2019", moneyTitle: "+10.00\nUSD", by: 0)
+            verifyCellLabels(with: "Funds Deposit\nJun 20, 2019", moneyTitle: "10.00\nUSD", by: 0)
         } else {
-            verifyCellLabels(with: "Funds Deposit Jun 20, 2019", moneyTitle: "+10.00 USD", by: 0)
+            verifyCellLabels(with: "Funds Deposit Jun 20, 2019", moneyTitle: "10.00 USD", by: 0)
         }
     }
 
