4c09cb119d8de2232b17cc20da63892a4eb1113b
Gustavo Meyer <gmeyer@hyperwallet.com>
2019-07-19 13:48:52 -0700

    Added if pod spec

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -16,11 +16,14 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#if !COCOAPODS
 import Common
-import HyperwalletSDK
-import os.log
 import Receipt
 import TransferMethod
+#endif
+import HyperwalletSDK
+import os.log
+
 import UIKit
 
 class HeadlineTableViewCell: UITableViewCell {
--- TransferMethod/Tests/SelectTransferMethodTypePresenterTests.swift
+++ TransferMethod/Tests/SelectTransferMethodTypePresenterTests.swift
@@ -1,4 +1,6 @@
+#if !COCOAPODS
 import Common
+#endif
 import Hippolyte
 import HyperwalletSDK
 @testable import TransferMethod
