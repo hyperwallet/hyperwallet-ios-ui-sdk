f917ebe35204b7f25fbde6f9d48c13c59c264838
Yurii Moskovko <yurii_moskovko@epam.com>
2019-07-30 22:40:48 +0300

    fixed section adding

--- Transfer/Sources/CreateTransferPresenter.swift
+++ Transfer/Sources/CreateTransferPresenter.swift
@@ -92,9 +92,8 @@ final class CreateTransferPresenter {
         )
         sectionData.append(createTransferDestinationSection)
 
-        if let currency = selectedTransferMethod?.transferMethodCurrency {
+        if selectedTransferMethod != nil {
             let createTransferSectionTransferData = CreateTransferSectionTransferData(
-                destinationCurrency: currency,
                 availableBalance: availableBalance
             )
             sectionData.append(createTransferSectionTransferData)
