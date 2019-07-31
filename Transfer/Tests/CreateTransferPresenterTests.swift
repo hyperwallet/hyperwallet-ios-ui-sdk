6b13f86b91dc85b409f5c035f2f12f27c55e9984
Yurii Moskovko <yurii_moskovko@epam.com>
2019-07-31 15:52:55 +0300

    added helpers for tests

--- Transfer/Tests/CreateTransferPresenterTests.swift
+++ Transfer/Tests/CreateTransferPresenterTests.swift
@@ -1,90 +1,113 @@
-//
-// Copyright 2018 - Present Hyperwallet
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-// and associated documentation files (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge, publish, distribute,
-// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or
-// substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+#if !COCOAPODS
+import Common
+#endif
 import Hippolyte
 import HyperwalletSDK
 @testable import Transfer
 import XCTest
 
 class TransferTests: XCTestCase {
+    private var presenter: CreateTransferPresenter!
+    private var mockView = MockCreateTransferView()
+
+    private let clientTransferId = "client-trasnsfer-id-123456789"
+    private let sourceToken = "trm-123456789"
+
     override func setUp() {
         Hyperwallet.setup(HyperwalletTestHelper.authenticationProvider)
-        //presenter = CreateTransferPresenter()
     }
 
     override func tearDown() {
         if Hippolyte.shared.isStarted {
             Hippolyte.shared.stop()
         }
-        //mockView.resetStates()
+        mockView.resetStates()
+    }
+
+    private func initializePresenterEmptySource() {
+        presenter = CreateTransferPresenter(clientTransferId, nil, view: mockView)
+        presenter.loadCreateTransfer()
+    }
+
+    private func initializePresenterWithSource() {
+        presenter = CreateTransferPresenter(clientTransferId, sourceToken, view: mockView)
+        presenter.loadCreateTransfer()
+    }
+
+    private func initializePresenterEmptyTransferMethods() {
+        let response = HyperwalletTestHelper.noContentHTTPResponse()
+        let url = String(format: "%@%@", HyperwalletTestHelper.userRestURL, "/transfer-methods?")
+        HyperwalletTestHelper.setUpMockServer(request:
+            HyperwalletTestHelper.buildGetRequestRegexMatcher(pattern: url, response))
+    }
+
+    func testInitializeSectionsWhenSelectedTransferMethodIsNil() {
+        initializePresenterEmptySource()
+        XCTAssertEqual(presenter.sectionData.count, 3, "")
+    }
+
+    func testInitializeSectionsWhenSelectedTransferMethodIsNotNil() {
+        initializePresenterEmptySource()
+        XCTAssertEqual(presenter.sectionData.count, 4, "")
     }
 }
 
 class MockCreateTransferView: CreateTransferView {
+    var isHideLoadingPerformed = false
+    var isNotifyTransferCreatedPerformed = false
+    var isShowCreateTransferPerformed = false
+    var isShowErrorPerformed = false
+    var isShowGenericTableViewPerformed = false
+    var isShowLoadingPerformed = false
+    var isShowScheduleTransferPerformed = false
+    var isUpdateTransferSectionPerformed = false
+
+    var expectation: XCTestExpectation?
+
+    func hideLoading() {
+        isHideLoadingPerformed = true
+    }
+
     func notifyTransferCreated(_ transfer: HyperwalletTransfer) {
+        isNotifyTransferCreatedPerformed = true
     }
 
     func showCreateTransfer() {
+        isShowCreateTransferPerformed = true
+    }
+
+    func showError(_ error: HyperwalletErrorType, _ retry: (() -> Void)?) {
+        isShowErrorPerformed = true
     }
 
     func showGenericTableView(items: [HyperwalletTransferMethod],
                               title: String,
                               selectItemHandler: @escaping CreateTransferView.SelectItemHandler,
                               markCellHandler: @escaping CreateTransferView.MarkCellHandler) {
-    }
-
-    func showScheduleTransfer(_ transfer: HyperwalletTransfer) {
-    }
-
-    func updateTransferSection() {
-    }
-
-    var isHideLoadingPerformed = false
-    var isShowLoadingPerformed = false
-    var isShowErrorPerformed = false
-    var isLoadReceiptPerformed = false
-
-    var expectation: XCTestExpectation?
-
-    func resetStates() {
-        isHideLoadingPerformed = false
-        isShowLoadingPerformed = false
-        isShowErrorPerformed = false
-        isLoadReceiptPerformed = false
-        expectation = nil
-    }
-
-    func hideLoading() {
-        isHideLoadingPerformed = true
-    }
-
-    func loadReceipts() {
-        isLoadReceiptPerformed = true
-        expectation?.fulfill()
-    }
-
-    func showError(_ error: HyperwalletErrorType, _ retry: (() -> Void)?) {
-        isShowErrorPerformed = true
-        retry!()
-        expectation?.fulfill()
+        isShowGenericTableViewPerformed = true
     }
 
     func showLoading() {
         isShowLoadingPerformed = true
+    }
+
+    func showScheduleTransfer(_ transfer: HyperwalletTransfer) {
+        isShowScheduleTransferPerformed = true
+    }
+
+    func updateTransferSection() {
+        isUpdateTransferSectionPerformed = true
+    }
+
+    func resetStates() {
+        isHideLoadingPerformed = false
+        isNotifyTransferCreatedPerformed = false
+        isShowCreateTransferPerformed = false
+        isShowErrorPerformed = false
+        isShowGenericTableViewPerformed = false
+        isShowLoadingPerformed = false
+        isShowScheduleTransferPerformed = false
+        isUpdateTransferSectionPerformed = false
+        expectation = nil
     }
 }
---
+++ Transfer/Tests/Helper/UserRepositoryRequestHelper.swift
@@ -0,0 +1,45 @@
+//
+// Copyright 2018 - Present Hyperwallet
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import Hippolyte
+import HyperwalletSDK
+import XCTest
+
+class UserRepositoryRequestHelper {
+    private static let userIndividualResponseFileName = "UserIndividualResponse"
+    private static let requestUrl = HyperwalletTestHelper.userRestURL
+
+    static func setupSucessRequest() {
+        let dataResponse = HyperwalletTestHelper.okHTTPResponse(for: userIndividualResponseFileName)
+        UserRepositoryRequestHelper.setUpMockServer(dataResponse, requestUrl)
+    }
+
+    static func setupFailureRequest() {
+        let errorResponse = HyperwalletTestHelper.badRequestHTTPResponse(for: userIndividualResponseFileName)
+        UserRepositoryRequestHelper.setUpMockServer(errorResponse, requestUrl)
+    }
+
+    static func getResponseError(_ error: HyperwalletErrorType) -> HyperwalletError {
+        return (error.getHyperwalletErrors()?.errorList?.first)!
+    }
+
+    private static func setUpMockServer(_ dataResponse: StubResponse, _ requestUrl: String) {
+        let dataRequest = HyperwalletTestHelper.buildGetRequest(baseUrl: requestUrl, dataResponse)
+        HyperwalletTestHelper.setUpMockServer(request: dataRequest)
+    }
+}
