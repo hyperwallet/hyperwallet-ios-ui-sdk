c1caa99cb61fe62451111205156721df1cda35b1
ddhar <ddhar@hyperwallet.com>
2019-09-17 13:15:00 -0700

    Fix swiftlint warning

--- Transfer/Sources/CreateTransferController.swift
+++ Transfer/Sources/CreateTransferController.swift
@@ -348,10 +348,13 @@ extension CreateTransferController: CreateTransferView {
 
     func showScheduleTransfer(_ transfer: HyperwalletTransfer) {
         if let transferMethod = presenter.selectedTransferMethod {
-            coordinator?.navigateToNextPage(initializationData:
-                [InitializationDataField.transfer: transfer,
-                 InitializationDataField.transferMethod: transferMethod,
-                 InitializationDataField.didFxQuoteChange: presenter.didFxQuoteChange])
+            coordinator?.navigateToNextPage(
+                initializationData: [
+                    InitializationDataField.transfer: transfer,
+                    InitializationDataField.transferMethod: transferMethod,
+                    InitializationDataField.didFxQuoteChange: presenter.didFxQuoteChange
+                ]
+            )
         }
     }
 }
