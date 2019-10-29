a3454f952fffc3facc4bd9d7d3f1404f188836b4
Debojit Dhar <32851891+ddhar-hw@users.noreply.github.com>
2019-10-28 17:09:48 -0700

    Task/hw 55471 insights add transfer method (#161)
    * HW-55471 : US2 - Add Transfer Method - Send event when transfer method options are loaded
    Initial commit to create a skeleton framework
    
    * Revert cartfile
    
    * Add class documentation

---
+++ Common/Sources/Insights/HyperwalletInsights.swift
@@ -0,0 +1,61 @@
+//
+// Copyright 2018 - Present Hyperwallet
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// import Insights
+
+/// Class responsible for initializing the Insights module.
+/// It contains methods to call Insights for various actions performed by the user
+public class HyperwalletInsights {
+    private static var instance: HyperwalletInsights?
+
+    /// Returns the previously initialized instance of the HyperwalletInsights interface object
+    public static var shared: HyperwalletInsights {
+        return instance ?? HyperwalletInsights()
+    }
+
+    /// Track Clicks
+    ///
+    /// - Parameters:
+    ///   - pageName: Name of the page
+    ///   - pageGroup: Page group name
+    ///   - link: The link clicked - example : select-transfer-method
+    ///   - params: A list of other information to be tracked - example : country,currency
+    public func trackClick(pageName: String, pageGroup: String, link: String, params: [String: String]) {
+    }
+
+    /// Track Error
+    ///
+    /// - Parameters:
+    ///   - pageName: Name of the page - example : transfer-method:add:select-transfer-method
+    ///   - pageGroup: Page group name - example : transfer-method
+    public func trackError(pageName: String, pageGroup: String) {
+    }
+
+    /// Track Impressions
+    ///
+    /// - Parameters:
+    ///   - pageName: Name of the page - example : transfer-method:add:select-transfer-method
+    ///   - pageGroup: Page group name - example : transfer-method
+    ///   - link: The link clicked - example : select-transfer-method
+    ///   - params: A list of other information to be tracked - example : country,currency
+    public func trackImpression(pageName: String, pageGroup: String, params: [String: String]) {
+    }
+
+    private func initializeInsights(completion: @escaping(Bool) -> Void) {
+    }
+}
