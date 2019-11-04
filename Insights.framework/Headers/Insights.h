99fed580a9025c1067db3164d94d2b14b10e3b6f
ddhar <ddhar@hyperwallet.com>
2019-11-04 10:48:34 -0800

    Add insights as a framework inside ui-sdk * No Carthage dependency * Framework added as binary

---
+++ Insights.framework/Headers/Insights.h
@@ -0,0 +1,19 @@
+//
+//  Insights.h
+//  Insights
+//
+//  Created by Dhar, Debojit on 10/11/19.
+//  Copyright © 2019 Hyperwallet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Insights.
+FOUNDATION_EXPORT double InsightsVersionNumber;
+
+//! Project version string for Insights.
+FOUNDATION_EXPORT const unsigned char InsightsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Insights/PublicHeader.h>
+
+
