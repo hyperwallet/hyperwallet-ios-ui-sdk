53575f9b01ee302911d583850347faf038bd5632
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2020-10-16 23:00:08 +0530

    Added Review Comment

--- TransferMethod/Sources/ListTransferMethodController.swift
+++ TransferMethod/Sources/ListTransferMethodController.swift
@@ -87,9 +87,8 @@ final class ListTransferMethodController: UITableViewController {
                                             UIColor(red: 0.17, green: 0.18, blue: 0.18, alpha: 1))
 
                 listTransferMethodCell.accessoryView = UIImageView(image: icon)
-                listTransferMethodCell.accessoryView?.isHidden = false
             } else {
-                listTransferMethodCell.accessoryView?.isHidden = true
+                listTransferMethodCell.accessoryView = nil
             }
         }
         return cell
