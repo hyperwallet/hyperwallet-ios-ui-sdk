a9ed02295247b99729169a7aa1a41cfa56785807
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2020-11-20 11:37:37 +0530

    Reverting

--- UITests/TransferMethod/AddTransferMethodBankAccountBusinessTests.swift
+++ UITests/TransferMethod/AddTransferMethodBankAccountBusinessTests.swift
@@ -75,6 +75,7 @@ class AddTransferMethodBankAccountBusinessTests: BaseTests {
                              method: HTTPMethod.post)
 
         waitForNonExistence(spinner)
+        waitForExistence(addTransferMethod.navBarBankAccount)
         XCTAssert(addTransferMethod.navBarBankAccount.exists)
 
         addTransferMethod.setBranchId("021000021")
