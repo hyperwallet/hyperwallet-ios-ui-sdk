7e53f0f22e9b8b4e6dd3e03c1992001d051af6ac
ddhar <ddhar@hyperwallet.com>
2020-11-20 09:57:39 -0800

    Refactor

--- Transfer/Sources/Helper/TransferAmountCurrencyFormatter.swift
+++ Transfer/Sources/Helper/TransferAmountCurrencyFormatter.swift
@@ -85,20 +85,21 @@ struct TransferAmountCurrencyFormatter {
         }
     }
 
-    static func formatCurrencyWithSymbolAndCode(_ amount: String, with currencyCode: String) -> String {
-        if let currencySymbol = TransferAmountCurrencyFormatter.getTransferAmountCurrency(for: currencyCode)?.symbol {
-            let formattedAmount = formatStringAmount(amount, with: currencyCode)
-            return String(format: "%@%@ %@", currencySymbol, formattedAmount, currencyCode)
-        } else {
-            return amount
-        }
-    }
-
+    /// Format currency amount by adding symbol and currency code
+    /// - Parameter currencyCode: currency code
+    /// - Returns: a formatted amount string
     static func addCurrencySymbolAndCode(_ amount: String, with currencyCode: String) -> String {
         if let currencySymbol = TransferAmountCurrencyFormatter.getTransferAmountCurrency(for: currencyCode)?.symbol {
             return String(format: "%@%@ %@", currencySymbol, amount, currencyCode)
         } else {
             return amount
         }
+    }
+
+    /// Format amount for currency code using users locale while adding symbol and currency code
+    /// - Parameter currencyCode: currency code
+    /// - Returns: a formatted amount string
+    static func formatCurrencyWithSymbolAndCode(_ amount: String, with currencyCode: String) -> String {
+        return addCurrencySymbolAndCode(formatStringAmount(amount, with: currencyCode), with: currencyCode)
     }
 }
