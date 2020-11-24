11d3a114fd7dd43e33dbf68e5f250b5ff26f4a8a
nanbehera <nbehera@paypal.com>
2020-11-25 02:47:39 +0530

    remove hardcoded font

--- Common/Sources/Helper/Theme.swift
+++ Common/Sources/Helper/Theme.swift
@@ -67,7 +67,7 @@ public class Theme: NSObject {
         /// The `UINavigationBar` large title font
         public static var largeTitleFont = UIFont.boldSystemFont(ofSize: 20.0)
         /// The `UINavigationBar` title font
-        public static var titleFont = UIFont.boldSystemFont(ofSize: 17.0)
+        public static var titleFont = UIFont.preferredFont(forTextStyle: .body)
     }
 
     /// Representation of all customizable visual style property for `UIButton`.
