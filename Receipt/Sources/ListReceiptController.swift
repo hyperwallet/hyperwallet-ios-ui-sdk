2838b26be32550dd76d8e58a437cd476bc7bb18e
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2020-11-26 18:26:18 +0530

    Dummy commit for travis

--- Receipt/Sources/ListReceiptController.swift
+++ Receipt/Sources/ListReceiptController.swift
@@ -177,6 +177,7 @@ extension ListReceiptController: ListReceiptView {
                                        for: .valueChanged)
             segmentedControl.selectedSegmentIndex = selectedSegmentedControl
             tableView.tableHeaderView = segmentedControl
+            // Add constraints to segmentedControl
             segmentedControl.translatesAutoresizingMaskIntoConstraints = false
             segmentedControl.leadingAnchor.constraint(equalTo: tableView.leadingAnchor, constant: 24)
                 .isActive = true
