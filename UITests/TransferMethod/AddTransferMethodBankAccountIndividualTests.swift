565ee84c5ef12247c96177b15520695aa0dd7cb5
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2021-03-01 09:37:59 +0530

    Wait time updated

--- UITests/TransferMethod/AddTransferMethodBankAccountIndividualTests.swift
+++ UITests/TransferMethod/AddTransferMethodBankAccountIndividualTests.swift
@@ -140,7 +140,7 @@ class AddTransferMethodBankAccountIndividualTests: BaseTests {
         mockServer.setupStubError(url: "/rest/v3/users/usr-token/bank-accounts",
                                   filename: "BankAccountInvalidRoutingResponse",
                                   method: HTTPMethod.post)
-        XCTAssertTrue(addTransferMethod.branchIdInput.waitForExistence(timeout: 40))
+        XCTAssertTrue(addTransferMethod.branchIdInput.waitForExistence(timeout: 35))
         addTransferMethod.setBranchId("021000022")
         addTransferMethod.setBankAccountId("12345")
         addTransferMethod.selectAccountType("CHECKING")
