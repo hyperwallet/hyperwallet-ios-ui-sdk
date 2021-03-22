fff9088a7ed2ce59d633e3d7ea8365e1ccec8aed
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2021-03-22 11:09:40 +0530

    Add transfer method icon updated.

--- Transfer/Sources/TransferDestinationCell.swift
+++ Transfer/Sources/TransferDestinationCell.swift
@@ -89,7 +89,7 @@ extension TransferDestinationCell {
         if !UIFont.isLargeSizeCategory {
             let icon = UIImage.fontIcon(hyperwalletIcon.rawValue,
                                         Theme.Icon.frame,
-                                        CGFloat(Theme.Icon.size),
+                                        CGFloat(20),
                                         Theme.Icon.primaryColor)
             imageView?.image = icon
             imageView?.layer.cornerRadius = CGFloat(Theme.Icon.frame.width / 2)
