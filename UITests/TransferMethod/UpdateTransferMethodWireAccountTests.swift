4da54d255b0d73c0e0162d48982a881d9e232aa2
aparthibanpaypal <71116197+aparthibanpaypal@users.noreply.github.com>
2021-05-20 14:15:26 +0530

    Removed the duplicates

--- UITests/TransferMethod/UpdateTransferMethodWireAccountTests.swift
+++ UITests/TransferMethod/UpdateTransferMethodWireAccountTests.swift
@@ -69,31 +69,4 @@ class UpdateTransferMethodWireAccountTests: BaseTests {
         XCTAssert(updateTransferMethod.elementQuery["city_error"].exists)
         XCTAssert(otherElements.containing(NSPredicate(format: "label CONTAINS %@", cityEmptyError)).count == 1)
     }
-    
-    func testUpdateTransferMethod_returWireAccountEmptyError1() {
-        mockServer.setupStub(url: "/rest/v3/users/usr-token/bank-accounts/trm-12345",
-                             filename: "WireAccountUpdateResponse",
-                             method: HTTPMethod.put)
-
-        updateTransferMethod.setCity("")
-        updateTransferMethod.clickUpdateTransferMethodButton()
-
-        XCTAssert(updateTransferMethod.elementQuery["city_error"].exists)
-        XCTAssert(otherElements.containing(NSPredicate(format: "label CONTAINS %@", cityEmptyError)).count == 1)
-    }
-    
-    func testUpdateTransferMethod_updateWireAccountValidResponse1() {
-        mockServer.setupStub(url: "/rest/v3/users/usr-token/bank-accounts/trm-12345",
-                             filename: "WireAccountUpdateResponse",
-                             method: HTTPMethod.put)
-
-        updateTransferMethod.selectCountry("United States")
-        updateTransferMethod.setStateProvince("MA")
-        updateTransferMethod.setCity("Lynn")
-        updateTransferMethod.setPostalCode("1905")
-        updateTransferMethod.clickUpdateTransferMethodButton()
-        waitForNonExistence(spinner)
-
-        XCTAssert(app.navigationBars["Transfer methods"].exists)
-    }
 }
